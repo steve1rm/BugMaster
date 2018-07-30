24860c0db818e0fc8564d89636b6fd8139b76026
= <stevekitkat@gmail.com>
2018-07-30 21:52:59 +0700

    working for diffUtil

--- app/src/main/kotlin/com/google/developer/bugmaster/presentation/adapters/InsectAdapter.kt
+++ app/src/main/kotlin/com/google/developer/bugmaster/presentation/adapters/InsectAdapter.kt
@@ -1,6 +1,8 @@
 package com.google.developer.bugmaster.presentation.adapters
 
+import android.support.v7.util.DiffUtil
 import com.google.developer.bugmaster.presentation.core.BaseDelegateAdapter
+import com.google.developer.bugmaster.presentation.core.InsectDiffCallback
 import com.google.developer.bugmaster.presentation.core.ItemDelegate
 
 class InsectAdapter(insectBugImageDelegate: ItemDelegate<CustomBugImageViewHolder, InsectAdapterItemType.InsectImage>,
@@ -10,23 +12,10 @@ class InsectAdapter(insectBugImageDelegate: ItemDelegate<CustomBugImageViewHolde
         insectDescriptionDelegate) {
 
     fun populateData(list: MutableList<InsectAdapterItemType>) {
+        val diffCallback = InsectDiffCallback(items.toMutableList(), list)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
         items = list
-        notifyDataSetChanged()
-
-//        val list: List<InsectAdapterItemType> = ArrayList()
-//        val insectDiffUtil: BaseDiffUtil<InsectAdapterItemType> = InsectDiffUtil()
- //       val oldItems = items
-/*
-        DiffUtil.calculateDiff(insectDiffUtil.callback(oldItems, list))
-                .dispatchUpdatesTo(this)
-*/
-
-       /* open fun applyData(list: List<PropertyDetailsItem>) {
-            val diffResult = DiffUtil.calculateDiff(propertyInfoDiffUtil.callback(items, list))
-            items = list
-            diffResult.dispatchUpdatesTo(this)
-        }
-        */
+        diffResult.dispatchUpdatesTo(this)
     }
 }
 
--- app/src/main/kotlin/com/google/developer/bugmaster/presentation/core/InsectDiffCallback.kt
+++ app/src/main/kotlin/com/google/developer/bugmaster/presentation/core/InsectDiffCallback.kt
@@ -1,21 +1,22 @@
 package com.google.developer.bugmaster.presentation.core
 
 import android.support.v7.util.DiffUtil
+import com.google.developer.bugmaster.presentation.adapters.InsectAdapterItemType
 
-class InsectDiffCallback : DiffUtil.Callback() {
+class InsectDiffCallback(private val oldInsectAdapterItemTypeViewModel: MutableList<InsectAdapterItemType>,
+                         private val newInsectAdapterItemTypeViewModel: MutableList<InsectAdapterItemType>)
+    : DiffUtil.Callback() {
+
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
         return oldItemPosition == newItemPosition
     }
 
-    override fun getOldListSize(): Int {
-        return 0
-    }
+    override fun getOldListSize() = oldInsectAdapterItemTypeViewModel.size
 
-    override fun getNewListSize(): Int {
-        return 0
-    }
+    override fun getNewListSize() = newInsectAdapterItemTypeViewModel.size
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldItemPosition == newItemPosition
+        return oldInsectAdapterItemTypeViewModel[oldItemPosition] ==
+                newInsectAdapterItemTypeViewModel[newItemPosition]
     }
 }
